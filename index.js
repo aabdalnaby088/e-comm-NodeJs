9c59f02af631b408c2debd84e12c790bc6fa1dd5
aabdalnaby088 <11410120210035@stud.cu.edu.eg>
2024-10-20 10:54:29 +0300

    adding stripe

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ import { AppError } from "./src/utils/appError.js"
 import { globalError } from "./src/middlewares/globalError.js"
 import { config } from "dotenv"
 import cors from 'cors'
+import Stripe from "stripe"
+import { catchError } from "./src/middlewares/catchError.js"
 const app = express()
 config(); 
 const port = process.env.PORT || 3000
+const stripe = new Stripe(process.env.SECRET_KEY);
+
+app.post('/api/webhook', express.raw({ type: 'application/json' }), catchError((req, res) => {
+    const sig = req.headers['stripe-signature'].toString();
+
+    let event;
+
+    event = stripe.webhooks.constructEvent(req.body, sig, "whsec_rzQAzsoSyYIl2rWvCw7FqhRAOzVAxGve");
+    let checkout 
+    // Handle the event
+    if (event.type == 'checkout.session.completed'){
+        checkout = event.data.object;
+    }
+    res.status(200).json({message: "success" ,checkout});
+}));
+
+
 app.use(cors());  
 app.use(express.json());
 app.use('/uploads' , express.static('uploads'))
